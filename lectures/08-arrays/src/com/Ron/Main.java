e200cfb8e7aed539184f6cf9f14e84ccfb1e0d4b
Ronnie <ronnieworkspace@gmail.com>
2024-07-19 21:51:04 +0530

    initialization & declaration of Arrays

--- lectures/08-arrays/src/com/Ron/Main.java
+++ lectures/08-arrays/src/com/Ron/Main.java
@@ -1,7 +1,38 @@
 package com.Ron;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Q: store a roll number
+        int a = 19;
+
+        // Q: store a person's name
+        String name = "Ronnie";
+
+        // Q: store 3 roll numbers
+        int rno1 = 23;
+        int rno2 = 55;
+        int rno3 = 18;
+
+        // * Syntax Of Array *
+        // datatype[] variable_name = new datatype[size];
+
+        // Q: store 5 roll numbers:
+//        int[] rnos = new int[5];
+//        // or directly
+//        int[] rnos2 = {23, 12, 45, 32, 15};
+
+        int[] ros; // declaration of array: ros is getting defined in the stack
+        ros = new int[5]; // initialisation: actually here object is being created in the memory (heap)
+
+//        System.out.println(ros[1]);
+
+        String[] arr = new String[4];
+        System.out.println(arr[0]);
+
+//        for (String element : arr) {
+//            System.out.println(element);
+//        }
+
     }
 }
